e7fc5516c2767bfcf231bc5b2603df8a484e8f35
Nick Martin <martin.nicholas.r@gmail.com>
2022-03-30 21:29:48 -0600

    homepage.handlebars rendering projects

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Project = require('../models/Project');
+const { Project, User } = require('../models');
 
 // route to get all dishes
 router.get('/', async (req, res) => {
@@ -11,5 +11,34 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(404)
     }
+});
+
+module.exports = router;
+
+
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
   
